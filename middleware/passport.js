e38c2a0a94c0600446b1dd75ee6d6827c303126e
dheeraj-deb <dheerajt9539@gmail.com>
2022-09-05 17:46:08 +0530

    admin-dashboard-salesreport-update

--- middleware/passport.js
+++ middleware/passport.js
@@ -11,7 +11,7 @@ passport.use(
       clientID:
         "323934525481-kjfv1vgg24qtrdtdjl0dbb6a0niso763.apps.googleusercontent.com", // Your Credentials here.
       clientSecret: "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", // Your Credentials here.
-      callbackURL: "https://fashioncluture.ml",
+      callbackURL: "https://fashioncluture.ml/auth/google/callback",
       passReqToCallback: true,
     },
     async function (request, accessToken, refreshToken, profile, done) {
